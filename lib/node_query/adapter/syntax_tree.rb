87ab46ae7c7f141887e0a1b1816f3e9c4324cd3d
Richard Huang <flyerhzm@gmail.com>
2023-06-26 18:42:42 +0800

    flatten syntax_tree children

--- lib/node_query/adapter/syntax_tree.rb
+++ lib/node_query/adapter/syntax_tree.rb
@@ -25,7 +25,7 @@ class NodeQuery::SyntaxTreeAdapter
   end
 
   def get_children(node)
-    node.deconstruct_keys([]).except(:location, :comments).values
+    node.deconstruct_keys([]).except(:location, :comments).values.flatten
   end
 
   def get_siblings(node)
