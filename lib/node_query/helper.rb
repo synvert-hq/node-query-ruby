df192528d17f8c1f88e5f8fbc75df3b64a8d31db
Richard Huang <flyerhzm@gmail.com>
2023-11-26 18:30:30 +0800

    add adapter to NodeQuery::Helper

--- lib/node_query/helper.rb
+++ lib/node_query/helper.rb
@@ -5,13 +5,14 @@ class NodeQuery::Helper
     # Get target node by the keys.
     # @param node [Node] ast node
     # @param keys [String|Array] keys of child node.
+    # @param adapter [NodeQuery::Adapter] adapter
     # @return [Node|] the target node.
-    def get_target_node(node, keys)
+    def get_target_node(node, keys, adapter)
       return unless node
 
       first_key, rest_keys = keys.to_s.split('.', 2)
       if node.is_a?(Array) && first_key === "*"
-        return node.map { |child_node| get_target_node(child_node, rest_keys) }
+        return node.map { |child_node| get_target_node(child_node, rest_keys, adapter) }
       end
 
       if node.is_a?(Array) && first_key =~ /\d+/
@@ -19,21 +20,22 @@ class NodeQuery::Helper
       elsif node.respond_to?(first_key)
         child_node = node.send(first_key)
       elsif first_key == "node_type"
-        child_node = NodeQuery.adapter.get_node_type(node)
+        child_node = adapter.get_node_type(node)
       end
 
       return child_node unless rest_keys
 
-      return get_target_node(child_node, rest_keys)
+      return get_target_node(child_node, rest_keys, adapter)
     end
 
     # Recursively handle child nodes.
     # @param node [Node] ast node
+    # @param adapter [NodeQuery::Adapter] adapter
     # @yield [child] Gives a child node.
     # @yieldparam child [Node] child node
-    def handle_recursive_child(node, &block)
-      NodeQuery.adapter.get_children(node).each do |child|
-        handle_child(child, &block)
+    def handle_recursive_child(node, adapter, &block)
+      adapter.get_children(node).each do |child|
+        handle_child(child, adapter, &block)
       end
     end
 
@@ -43,18 +45,19 @@ class NodeQuery::Helper
     #     evaluated_node_value(node, "@{{value}}") # => @id
     # @param node [Node] ast node
     # @param str [String] string to be evaluated
+    # @param adapter [NodeQuery::Adapter] adapter
     # @return [String] evaluated string
-    def evaluate_node_value(node, str)
+    def evaluate_node_value(node, str, adapter)
       str.scan(/{{(.+?)}}/).each do |match_data|
-        target_node = NodeQuery::Helper.get_target_node(node, match_data.first)
-        str = str.sub("{{#{match_data.first}}}", to_string(target_node))
+        target_node = NodeQuery::Helper.get_target_node(node, match_data.first, adapter)
+        str = str.sub("{{#{match_data.first}}}", to_string(target_node, adapter))
       end
       str
     end
 
-    def to_string(node)
-      if NodeQuery.adapter.is_node?(node)
-        return NodeQuery.adapter.get_source(node)
+    def to_string(node, adapter)
+      if adapter.is_node?(node)
+        return adapter.get_source(node)
       end
 
       node.to_s
@@ -62,13 +65,13 @@ class NodeQuery::Helper
 
     private
 
-    def handle_child(node, &block)
-      if NodeQuery.adapter.is_node?(node)
+    def handle_child(node, adapter, &block)
+      if adapter.is_node?(node)
         block.call(node)
-        handle_recursive_child(node, &block)
+        handle_recursive_child(node, adapter, &block)
       elsif node.is_a?(Array)
         node.each do |child_node|
-          handle_child(child_node, &block) unless child_node.nil?
+          handle_child(child_node, adapter, &block) unless child_node.nil?
         end
       end
     end
--- spec/node_query/helper_spec.rb
+++ spec/node_query/helper_spec.rb
@@ -6,32 +6,32 @@ RSpec.describe NodeQuery::Helper do
   describe '.get_target_node' do
     it 'checks node_type' do
       node = parse('Factory.define :user do |user|; end')
-      child_node = described_class.get_target_node(node, 'node_type')
+      child_node = described_class.get_target_node(node, 'node_type', NodeQuery::ParserAdapter.new)
       expect(child_node).to eq :block
     end
 
     context 'block node' do
       it 'checks caller' do
         node = parse('Factory.define :user do |user|; end')
-        child_node = described_class.get_target_node(node, 'caller')
+        child_node = described_class.get_target_node(node, 'caller', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.caller
       end
 
       it 'checks arguments' do
         node = parse('Factory.define :user do |user|; end')
-        child_node = described_class.get_target_node(node, 'arguments')
+        child_node = described_class.get_target_node(node, 'arguments', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.arguments
       end
 
       it 'checks caller.receiver' do
         node = parse('Factory.define :user do |user|; end')
-        child_node = described_class.get_target_node(node, 'caller.receiver')
+        child_node = described_class.get_target_node(node, 'caller.receiver', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.caller.receiver
       end
 
       it 'checks caller.message' do
         node = parse('Factory.define :user do |user|; end')
-        child_node = described_class.get_target_node(node, 'caller.message')
+        child_node = described_class.get_target_node(node, 'caller.message', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.caller.message
       end
     end
@@ -39,13 +39,13 @@ RSpec.describe NodeQuery::Helper do
     context 'array' do
       it 'checks array by index' do
         node = parse('factory :admin, class: User do; end')
-        child_node = described_class.get_target_node(node, 'caller.arguments.1')
+        child_node = described_class.get_target_node(node, 'caller.arguments.1', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.caller.arguments[1]
       end
 
       it 'checks array by method' do
         node = parse('factory :admin, class: User do; end')
-        child_node = described_class.get_target_node(node, 'caller.arguments.first')
+        child_node = described_class.get_target_node(node, 'caller.arguments.first', NodeQuery::ParserAdapter.new)
         expect(child_node).to eq node.caller.arguments.first
       end
     end
@@ -55,7 +55,7 @@ RSpec.describe NodeQuery::Helper do
     it 'recursively handle all children' do
       node = parse('class Synvert; def current_node; @node; end; end')
       children = []
-      described_class.handle_recursive_child(node) { |child| children << child.type }
+      described_class.handle_recursive_child(node, NodeQuery::ParserAdapter.new) { |child| children << child.type }
       expect(children).to be_include :const
       expect(children).to be_include :def
       expect(children).to be_include :args
@@ -66,7 +66,7 @@ RSpec.describe NodeQuery::Helper do
   describe '.evaluate_node_value' do
     it 'returns an evaluated string' do
       node = parse('@id = id')
-      value = described_class.evaluate_node_value(node, "@{{value}}")
+      value = described_class.evaluate_node_value(node, "@{{value}}", NodeQuery::ParserAdapter.new)
       expect(value).to eq '@id'
     end
   end
@@ -74,11 +74,11 @@ RSpec.describe NodeQuery::Helper do
   describe '.to_string' do
     it 'gets source code of the node' do
       node = parse('@id = id')
-      expect(described_class.to_string(node)).to eq '@id = id'
+      expect(described_class.to_string(node, NodeQuery::ParserAdapter.new)).to eq '@id = id'
     end
 
     it 'gets string' do
-      expect(described_class.to_string(true)).to eq 'true'
+      expect(described_class.to_string(true, NodeQuery::ParserAdapter.new)).to eq 'true'
     end
   end
 end
