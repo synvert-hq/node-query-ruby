ca977ba4658cd0d4a31d1c6cd72784ccc32d3afa
Richard Huang <flyerhzm@gmail.com>
2024-02-13 15:22:42 +0800

    use Hash#filter instead of Hash#reject

--- lib/node_query/adapter/syntax_tree.rb
+++ lib/node_query/adapter/syntax_tree.rb
@@ -3,14 +3,6 @@
 require 'syntax_tree'
 require 'syntax_tree_ext'
 
-if RUBY_VERSION.to_i < 3
-  class Hash
-    def except(*keys)
-      self.reject { |k, _| keys.include?(k) }
-    end
-  end
-end
-
 class NodeQuery::SyntaxTreeAdapter
   def is_node?(node)
     node.is_a?(SyntaxTree::Node)
@@ -25,7 +17,7 @@ class NodeQuery::SyntaxTreeAdapter
   end
 
   def get_children(node)
-    node.deconstruct_keys([]).except(:location, :comments).values
+    node.deconstruct_keys([]).filter { |key, _value| ![:location, :comments].include?(key) }.values
   end
 
   def get_siblings(node)
